336f90885f23e580a78006855afe6a865d5ae3d9
govidat <govidat@gmail.com>
2015-02-07 16:33:52 +0530

    initialize model

---
+++ tests/fixtures/todo.js
@@ -0,0 +1,5 @@
+export default [
+  { id: "1", title: 'learn bootstrap', isCompleted: true },
+  { id: "2", title: 'learn api in ror', isCompleted: false },
+  { id: "3", title: 'link ember and ror', isCompleted: false }
+];
