ad47c7b89ca8b0968eb96d2ea9f1f6e3cbad7378
Jeff Schilling <jeff.schilling@q2ebanking.com>
2015-04-07 21:56:22 -0400

    minor updates to blanket options format

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -1,18 +1,17 @@
-/* globals blanket, exports */
-var options = {
-    modulePrefix: "todomvc-ember-cli",
-    filter: "//.*todomvc-ember-cli/.*/",
-    antifilter: "//.*(tests|template).*/",
-    modulePattern: "\/(\\w+)",
-    branchTracking: true,
-    loaderExclusions: [],
-    enableCoverage: true,
-    cliOptions: {
-      outputFile: 'test-output.json',
-      reporters: ['json']
-    }
-  };
+/*globals blanket */
 
+var options = {
+   modulePrefix: "todomvc-ember-cli",
+   filter: "//.*todomvc-ember-cli/.*/",
+   antifilter: "//.*(tests|template).*/",
+   loaderExclusions: [],
+   enableCoverage: true,
+   cliOptions: {
+    outputFile: 'test-output.json',
+    reporters: ['json']
+  }
+
+};
 if (typeof exports === 'undefined') {
   blanket.options(options);
 } else {
